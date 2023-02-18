d0b43acef30b8bd28cffbb6a34c3780f81643a02
ArjhanToteck <124912029+ArjhanT0teck@users.noreply.github.com>
2023-02-18 00:10:24 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
-if(typeof document.getElementById('save') !=="undefined"){
-function downloadURI(d, e) {
+var lol=0;
+if(location.href==="https://pixlr.com/x/#editor"&&lol===0){
+    lol++
+document.getElementById('save').onclick=function(){
+setTimeout(function(){
+    function downloadURI(d, e) {
     var o = document.createElement("a");
     o.download = e, o.href = d, document.body.appendChild(o), o.click(), document.body.removeChild(o)
 }
-document.getElementById('save').textContent="download"
-
-document.getElementById('save').onclick=function(){
-    downloadURI(document.getElementsByClassName("canvas")[0].toDataURL())
-    // will add panel later so you can change what you're downloading it as
-    setTimeout(function(){
-    document.getElementsByClassName("modal dim")[0].remove()
-    },100)
-
+const canvas=document.getElementsByTagName("canvas")[0];
+let canvasW = canvas.getBoundingClientRect().width;
+let canvasH = canvas.getBoundingClientRect().height;
+let btnC="button big white w-100 pad-30 text-default"
+document.getElementsByClassName('heavy bold')[0].parentNode.innerHTML=`
+<span><img src="${document.getElementsByClassName("canvas")[0].toDataURL()}" width="${canvasW/10}" height="${canvasH/10}"><h1 class="heavy bold" onclick="window.open('https://github.com/ArjhanT0teck/free-pixlr')">Free pixlr</h1></span>
+<a href="https://github.com/ArjhanT0teck/free-pixlr/">Github</a>
+Download as:
+<button onclick="downloadURI('${document.getElementsByClassName("canvas")[0].toDataURL()}','pixlr.jpg')" class="${btnC}">JPG</button>
+<button class="${btnC}" onclick="downloadURI('${document.getElementsByClassName("canvas")[0].toDataURL()}','pixlr.svg')">SVG</button>
+<button class="${btnC}" onclick="downloadURI('${document.getElementsByClassName("canvas")[0].toDataURL()}','pixlr.png')">PNG</button>
+`
+},100)
 }
 } else {
-    alert("You must use it inside of a image editor")
+    alert("Error either go into the editor or press save.")
 }
